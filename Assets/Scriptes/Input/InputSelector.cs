f9189264e7af084fe3d95e9af5cd207329c4168f
Artem <av.unitygames@gmail.com>
2021-05-04 22:24:46 +0300

    Added mobile and keyboard movement

---
+++ Assets/Scriptes/Input/InputSelector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSelector : MonoBehaviour
+{
+    [SerializeField] private Snake _moveableObject;
+    [SerializeField] private BaseInput _mobileInput;
+    [SerializeField] private BaseInput _keyboardInput;
+
+    private void Start()
+    {
+#if UNITY_EDITOR
+        _keyboardInput.Init(_moveableObject as IMoveable);
+#else
+        _mobileInput.Init(_moveableObject);
+#endif
+    }
+}
--- Assets/Scriptes/Snake.cs
+++ Assets/Scriptes/Snake.cs
@@ -2,7 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Snake : MonoBehaviour
+public class Snake : MonoBehaviour, IMoveable
 {
+    private float _speed;
 
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+    }
+
+    public void EndMove()
+    {
+        _speed = 0f;
+    }
+
+    public void StartMove()
+    {
+        _speed = 5f;
+    }
 }
