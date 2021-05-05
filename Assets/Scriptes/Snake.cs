98bd881e931eacebb598eeea019b7f0a4a80bf88
Cromwill <33699699+Cromwill@users.noreply.github.com>
2021-05-05 20:29:04 +0300

    Create road and path

--- Assets/Scriptes/Snake.cs
+++ Assets/Scriptes/Snake.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
+    [SerializeField] private float _speedTime;
+    [SerializeField] private Track _track;
+
+    private Transform _selfTransform;
+    
+
+    private void Start()
+    {
+        _selfTransform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        _selfTransform.position = _track.GetPosition(1 / _speedTime * Time.deltaTime);
+    }
+
 
 }
