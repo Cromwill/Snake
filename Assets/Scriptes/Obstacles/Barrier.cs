61d8e3112afc9c7ae6703528a42b583295710156
Cromwill <33699699+Cromwill@users.noreply.github.com>
2021-05-07 17:04:39 +0300

    Add Obstacle class

---
+++ Assets/Scriptes/Obstacles/Barrier.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier : Obstacle
+{
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private Direction _direction;
+
+    private Transform _selfTransform;
+
+    void Start()
+    {
+        _selfTransform = GetComponent<Transform>();
+    }
+
+
+    void Update()
+    {
+        _selfTransform.Rotate(0, _rotationSpeed * Time.deltaTime * (float)_direction, 0);
+    }
+
+}
+
+public enum Direction
+{
+    Left = -1,
+    Right = 1
+}
