78fcc6a80dcb78d6cc3c9b21b63af8f2308ca1bd
Cromwill <33699699+Cromwill@users.noreply.github.com>
2021-05-10 20:00:03 +0300

    Add UI

---
+++ Assets/Scriptes/GameDataStorage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public static class GameDataStorage
+{
+    private const string PROGRESS_KEY = "ProgressLevel";
+    private const string GEMCOUNT_KEY = "GemCount";
+
+    public static void SaveProgress(int level)
+    {
+        SaveInt(PROGRESS_KEY, level);
+    }
+
+    public static void SaveGemCount(int gem)
+    {
+        SaveInt(GEMCOUNT_KEY, gem);
+    }
+
+    public static int LoadProgress() => LoadInt(PROGRESS_KEY);
+
+    public static int LoadGemCount() => LoadInt(GEMCOUNT_KEY);
+
+    private static void SaveInt(string key, int value)
+    {
+        PlayerPrefs.SetInt(key, value);
+        PlayerPrefs.Save();
+    }
+
+    private static int LoadInt(string key)
+    {
+        return PlayerPrefs.GetInt(key);
+    }
+}
+
---
+++ Assets/Scriptes/UI/ProgressView.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[RequireComponent(typeof(Slider))]
+public class ProgressView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _currentLevelView;
+    [SerializeField] private TMP_Text _nextLevelView;
+    [SerializeField] private TMP_Text _gemCountView;
+
+    private Slider _progress;
+    private Track _track;
+
+    private void Start()
+    {
+        _progress = GetComponent<Slider>();
+        _track = FindObjectOfType<Track>();
+
+        int levelNumber = GameDataStorage.LoadProgress();
+
+        _currentLevelView.SetText(levelNumber.ToString());
+        _nextLevelView.SetText((levelNumber + 1).ToString());
+        _gemCountView.SetText(GameDataStorage.LoadGemCount().ToString());
+    }
+
+    private void Update()
+    {
+        _progress.value = _track.PlayerDistanceTraveleds;
+    }
+
+}
