380e85dd5cc5db92b1eb68b33abf2be7e681984d
Cromwill <33699699+Cromwill@users.noreply.github.com>
2021-05-10 22:11:47 +0300

    Add pillars and knife

--- Assets/Scriptes/Obstacles/Knife.cs
+++ Assets/Scriptes/Obstacles/Knife.cs
@@ -5,29 +5,28 @@ public class Knife : MonoBehaviour
     [SerializeField] private float _loweringSpeed;
     [SerializeField] private float _liftingSpeed;
     [SerializeField] private float _delayTime;
+    [SerializeField] private Direction _direction;
 
     private Transform _selfTransform;
-    private Direction _direction;
 
     private const float MIN_Z_ROTATION = 270;
 
     private void Start()
     {
         _selfTransform = GetComponent<Transform>();
-        _direction = Direction.Left;
     }
 
 
     private void Update()
     {
-        if (_delayTime <= 0)
-        {
-            var speed = _direction == Direction.Left ? _loweringSpeed : _liftingSpeed;
-            _selfTransform.Rotate(0, 0, speed * (int)_direction * Time.deltaTime, Space.World);
-            CheckDirection();
-        }
-        else
-            _delayTime -= Time.deltaTime;
+        //if (_delayTime <= 0)
+        //{
+        //    var speed = _direction == Direction.Left ? _loweringSpeed : _liftingSpeed;
+        //    _selfTransform.Rotate(0, 0, speed * (int)_direction * Time.deltaTime, Space.World);
+        //    CheckDirection();
+        //}
+        //else
+        //    _delayTime -= Time.deltaTime;
     }
 
     private void CheckDirection()
@@ -35,9 +34,18 @@ public class Knife : MonoBehaviour
         if (_selfTransform.rotation.eulerAngles.z != 0)
         {
             if (_direction == Direction.Left && _selfTransform.rotation.eulerAngles.z < MIN_Z_ROTATION)
+            {
                 _direction = Direction.Right;
-            else if (_direction == Direction.Right && _selfTransform.rotation.eulerAngles.z > 359)
+                Vector3 rotation = _selfTransform.rotation.eulerAngles;
+                _selfTransform.rotation = Quaternion.Euler(rotation.x, rotation.y, MIN_Z_ROTATION);
+            }
+            else if (_direction == Direction.Right && _selfTransform.rotation.eulerAngles.z > 0 &&
+                _selfTransform.rotation.eulerAngles.z < 270)
+            {
                 _direction = Direction.Left;
+                Vector3 rotation = _selfTransform.rotation.eulerAngles;
+                _selfTransform.rotation = Quaternion.Euler(rotation.x, rotation.y, 0);
+            }
         }
     }
 }
