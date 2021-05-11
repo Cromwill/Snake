ae5b1f62fbb8827d6a9543ab1a35f89f08f783d5
Cromwill <33699699+Cromwill@users.noreply.github.com>
2021-05-12 00:02:42 +0300

    Change Scene4

--- Assets/Scriptes/Obstacles/Obstacle.cs
+++ Assets/Scriptes/Obstacles/Obstacle.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    [SerializeField] private MeshRenderer _obstacleSignal;
+    [SerializeField] private MeshRenderer[] _obstacleSignals;
     [SerializeField] private Material _workingMaterial;
     [SerializeField] private Material _notWorkingMaterial;
 
     public void ToggleSignal()
     {
-        if (_obstacleSignal != null)
-            _obstacleSignal.material = _notWorkingMaterial;
+        if (_obstacleSignals != null && _obstacleSignals.Length > 0)
+        {
+            foreach (var signal in _obstacleSignals)
+                signal.material = _notWorkingMaterial;
+        }
     }
 }
