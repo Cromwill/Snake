91b6af15db67d50d0506f7c75d0a0a0f63dc873d
Artem <av.unitygames@gmail.com>
2021-05-12 19:16:45 +0300

    Improve snake bone scaler

--- Assets/Scriptes/Snake/SnakeTrigger/Tail.cs
+++ Assets/Scriptes/Snake/SnakeTrigger/Tail.cs
@@ -7,6 +7,10 @@ public class Tail : SnakeTrigger
     public void LateUpdate()
     {
         Vector3 parentScale = transform.parent.lossyScale;
+
+        if (parentScale.x == 0 || parentScale.y == 0 || parentScale.z == 0)
+            return;
+
         transform.localScale = new Vector3(1f / parentScale.x, 1f / parentScale.y, 1f / parentScale.z);
     }
 }
