8f8529eb97fb8fc5cff724e294aa0ad8dfddd6fd
Cromwill <33699699+Cromwill@users.noreply.github.com>
2021-05-19 02:20:00 +0300

    Disable barrier damage

--- Assets/Scriptes/Obstacles/ObstacleExitTrigger.cs
+++ Assets/Scriptes/Obstacles/ObstacleExitTrigger.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 
 public class ObstacleExitTrigger : MonoBehaviour
 {
+    [SerializeField] private Obstacle _obstacle;
+
     public event Action TriggerExit;
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.TryGetComponent(out Head head))
+        if (other.TryGetComponent(out Head head))
+        {
             TriggerExit?.Invoke();
+            if (_obstacle != null)
+                _obstacle.DisableObstacle();
+        }
     }
 }
