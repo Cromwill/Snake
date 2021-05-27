8ac0d3d2b7946fba2533ca36a4606f91b383ccde
Cromwill <33699699+Cromwill@users.noreply.github.com>
2021-05-27 13:42:43 +0300

    Add numbers Textures

---
+++ Assets/Scriptes/UI/ToggleWater.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ToggleWater : MonoBehaviour
+{
+    [SerializeField] private GameObject _toonWater;
+    [SerializeField] private GameObject _realWater;
+
+    private void Start()
+    {
+        if (_toonWater.activeSelf && _realWater.activeSelf)
+            _realWater.SetActive(false);
+        if (!_toonWater.activeSelf && !_realWater.activeSelf)
+            _toonWater.SetActive(true);
+    }
+
+    public void ToogleWater()
+    {
+        _toonWater.SetActive(!_toonWater.activeSelf);
+        _realWater.SetActive(!_realWater.activeSelf);
+    }
+}
