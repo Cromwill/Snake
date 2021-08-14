09db3549aad992053759678b0764386fa2413c76
Artem <av.unitygames@gmail.com>
2021-08-14 16:56:57 +0300

    Update hat data base

--- Assets/Scriptes/UI/HatCollection/HatCollectionItemPresenter.cs
+++ Assets/Scriptes/UI/HatCollection/HatCollectionItemPresenter.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class HatCollectionItemPresenter : MonoBehaviour
 {
+    [SerializeField] private Image _border;
     [SerializeField] private Image _selfImage;
     [SerializeField] private Image _focus;
     [SerializeField] private Sprite _unlockedBorder;
@@ -18,13 +19,16 @@ public class HatCollectionItemPresenter : MonoBehaviour
             return;
         }
 
+        _border.sprite = _unlockedBorder;
+        _selfImage.enabled = true;
         _selfImage.sprite = data.HatPreview;
         _focus.enabled = true;
     }
 
     public void RenderLocked()
     {
-        _selfImage.sprite = _lockedBorder;
+        _border.sprite = _lockedBorder;
+        _selfImage.enabled = false;
         _focus.enabled = false;
     }
 }
