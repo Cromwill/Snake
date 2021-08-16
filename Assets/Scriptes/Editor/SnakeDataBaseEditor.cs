1645d7e998f510ffcbfb23fe67d2e04132668ef2
Artem <av.unitygames@gmail.com>
2021-08-16 13:32:27 +0300

    Update data base editor

--- Assets/Scriptes/Editor/SnakeDataBaseEditor.cs
+++ Assets/Scriptes/Editor/SnakeDataBaseEditor.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.Linq;
 using System.Reflection;
 using UnityEditor;
+using System.Text;
 
 [CustomEditor(typeof(SnakeDataBase))]
 public class SnakeDataBaseEditor : Editor
@@ -11,20 +12,18 @@ public class SnakeDataBaseEditor : Editor
     private SnakeDataBase _dataBase;
     private SerializedProperty _dataBaseList;
     private FieldInfo _dataBaseListInfo;
-    private List<string> _boosterDataFields;
+    private int _currentRenderIndex;
+    private GameObject _previewGameObject;
+    private Editor _previewGameObjectEditor;
 
     private void OnEnable()
     {
         _dataBase = target as SnakeDataBase;
 
-        _boosterDataFields = new List<string>();
-        foreach (var field in typeof(SnakeData).GetFields(BindingFlags.NonPublic | BindingFlags.Instance))
-            _boosterDataFields.Add(field.Name);
-        foreach (var field in typeof(GUIDData).GetFields(BindingFlags.NonPublic | BindingFlags.Instance))
-            _boosterDataFields.Add(field.Name);
-
         FieldInfo[] allFields = _dataBase.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
         _dataBaseListInfo = allFields.ToList().Find((field) => field.FieldType.IsEquivalentTo(typeof(List<SnakeData>)));
+
+        _currentRenderIndex = 0;
     }
 
     public override void OnInspectorGUI()
@@ -33,25 +32,118 @@ public class SnakeDataBaseEditor : Editor
 
         _dataBaseList = serializedObject.FindProperty(_dataBaseListInfo.Name);
 
-        for (int i = 0; i < _dataBaseList.arraySize; i++)
+        GUIStyle headerTextStyle = new GUIStyle();
+        headerTextStyle.fontSize = 35;
+        headerTextStyle.normal.textColor = Color.green;
+
+        GUIStyle defaultTextStyle = new GUIStyle();
+        defaultTextStyle.fontSize = 18;
+        defaultTextStyle.normal.textColor = Color.white;
+
+        GUILayout.BeginVertical();
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        EditorGUILayout.LabelField("SNAKE DATA BASE", headerTextStyle);
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+        GUILayout.Space(50);
+
+        var failedIndexes = GetFailedIndexes(_dataBaseList);
+        if (failedIndexes.Count > 0)
         {
-            SerializedProperty element = _dataBaseList.GetArrayElementAtIndex(i);
-            foreach (var item in _boosterDataFields)
-                EditorGUILayout.PropertyField(element.FindPropertyRelative(item));
+            StringBuilder value = new StringBuilder();
+            foreach (var index in failedIndexes)
+                value.Append(index + ", ");
 
-            if (GUILayout.Button(new GUIContent("-", "Удалить")))
-                _dataBase.RemoveAt(i);
-
-            if (GUILayout.Button(new GUIContent("Up", "Сместить к началу")))
-                _dataBase.MoveFront(i);
-            if (GUILayout.Button(new GUIContent("Down", "Сместить в конец")))
-                _dataBase.MoveBack(i);
+            EditorGUILayout.HelpBox("Не вся база заполнена!\nПроверьте следующие элементы:\n" + value, MessageType.Warning);
         }
 
-        if (GUILayout.Button(new GUIContent("+", "Добавить")))
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("_defaultSnakeIndex"));
+        GUILayout.Space(20);
+
+        EditorGUILayout.LabelField("Hat count: " + _dataBaseList.arraySize, defaultTextStyle);
+        GUILayout.FlexibleSpace();
+
+        GUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Current hat index: " + _currentRenderIndex, defaultTextStyle);
+
+        if (GUILayout.Button(new GUIContent("<", "Предыдущий элемент"), GUILayout.Width(30), GUILayout.Height(30)))
+            _currentRenderIndex = (_currentRenderIndex > 0) ? _currentRenderIndex - 1 : _currentRenderIndex;
+        if (GUILayout.Button(new GUIContent(">", "Следующий элемент"), GUILayout.Width(30), GUILayout.Height(30)))
+            _currentRenderIndex = (_currentRenderIndex < _dataBaseList.arraySize - 1) ? _currentRenderIndex + 1 : _currentRenderIndex;
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button(new GUIContent("+", "Добавить"), GUILayout.Width(30), GUILayout.Height(30)))
             _dataBase.Add(new SnakeData());
+        if (GUILayout.Button(new GUIContent("-", "Удалить"), GUILayout.Width(30), GUILayout.Height(30)))
+            _dataBase.RemoveAt(_currentRenderIndex);
+
+        if (_currentRenderIndex >= _dataBase.Data.Count())
+            _currentRenderIndex = _dataBase.Data.Count() - 1;
+
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(30);
+        var element = _dataBaseList.GetArrayElementAtIndex(_currentRenderIndex);
+        RenderElement(element);
+        GUILayout.EndVertical();
 
         serializedObject.ApplyModifiedProperties();
+    }
+
+    private void RenderElement(SerializedProperty element)
+    {
+        EditorGUILayout.PropertyField(element.FindPropertyRelative("_preview"));
+        EditorGUILayout.PropertyField(element.FindPropertyRelative("_name"));
+        EditorGUILayout.PropertyField(element.FindPropertyRelative("_description"));
+        EditorGUILayout.PropertyField(element.FindPropertyRelative("_price"));
+        EditorGUILayout.PropertyField(element.FindPropertyRelative("_prefab"));
+        EditorGUILayout.PropertyField(element.FindPropertyRelative("_previewModel"));
+
+        var sprite = element.FindPropertyRelative("_preview").objectReferenceValue as Sprite;
+        var preview = element.FindPropertyRelative("_previewModel").objectReferenceValue as GameObject;
+
+        GUILayout.Space(50);
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        if (sprite)
+            GUILayout.Label(sprite.texture, GUILayout.Width(128), GUILayout.Height(128));
+
+        GUILayout.FlexibleSpace();
+
+        if (preview)
+        {
+            if (_previewGameObject == null || preview.Equals(_previewGameObject) == false)
+                _previewGameObjectEditor = Editor.CreateEditor(preview);
+
+            _previewGameObject = preview;
+
+            if (_previewGameObject != null)
+                _previewGameObjectEditor.OnInteractivePreviewGUI(GUILayoutUtility.GetRect(200, 200), GUIStyle.none);
+        }
+
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+    }
+
+    private List<int> GetFailedIndexes(SerializedProperty array)
+    {
+        var failedList = new List<int>();
+
+        for (int i = 0; i < array.arraySize; i++)
+        {
+            var fieldList = new List<object>();
+            var element = _dataBaseList.GetArrayElementAtIndex(i);
+
+            fieldList.Add(element.FindPropertyRelative("_preview").objectReferenceValue);
+            fieldList.Add(element.FindPropertyRelative("_prefab").objectReferenceValue);
+            fieldList.Add(element.FindPropertyRelative("_previewModel").objectReferenceValue);
+
+            if (fieldList.Any(field => field == null))
+                failedList.Add(i);
+        }
+
+        return failedList;
     }
 }
 #endif
