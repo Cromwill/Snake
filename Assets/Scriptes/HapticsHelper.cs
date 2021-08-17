d25ab43cb135ad9ec1dea0942329a63151a5db96
Cromwill <33699699+Cromwill@users.noreply.github.com>
2021-08-17 11:35:42 +0300

    Add vibrations

---
+++ Assets/Scriptes/HapticsHelper.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using MoreMountains.NiceVibrations;
+
+public class HapticsHelper : MonoBehaviour
+{
+    [SerializeField] private SnakeInitializer _initializer;
+
+    private Head _snakeHead;
+
+    private void OnEnable()
+    {
+        _initializer = FindObjectOfType<SnakeInitializer>();
+        _initializer.Initialized += OnSnakeInitializer;
+    }
+
+    private void OnDisable()
+    {
+#if UNITY_IOS
+        MMNViOS.iOSReleaseHaptics();
+#endif
+
+        _initializer.Initialized -= OnSnakeInitializer;
+        if (_snakeHead != null)
+        {
+            _snakeHead.FoodFinded -= OnFoodFinded;
+            _snakeHead.ObstacleEntered -= OnObstacleEntered;
+        }
+    }
+
+    private void Awake()
+    {
+#if UNITY_IOS
+        MMNViOS.iOSInitializeHaptics();
+#endif
+    }
+
+    private void OnSnakeInitializer(Snake snake)
+    {
+        _snakeHead = snake.GetComponentInChildren<Head>();
+        _snakeHead.FoodFinded += OnFoodFinded;
+        _snakeHead.ObstacleEntered += OnObstacleEntered;
+    }
+
+    private void OnFoodFinded(Food food)
+    {
+        Debug.Log("Food finded");
+        MMVibrationManager.Haptic(HapticTypes.SoftImpact);
+    }
+
+    private void OnObstacleEntered(Obstacle obstacle)
+    {
+        Debug.Log("Obstacle Intered");
+        MMVibrationManager.Haptic(HapticTypes.Warning);
+    }
+}
